9fdf4e150bd3d855c4781ba0f8daafeacefbedb9
ThatBaldDude4 <taylorv0004@gmail.com>
2026-04-22 17:05:39 -0500

    add isSunk() method to ship class

--- ship.js
+++ ship.js
@@ -2,10 +2,16 @@ class Ship {
     constructor(length) {
         this.length = length;
         this.hits = 0;
-        this.isSunk = false;
+        this.sunk = false;
     };
 
     hit() {
         this.hits += 1;
+    }
+
+    isSunk() {
+        let result = this.hits >= this.length
+        this.sunk = result;
+        return result;
     }
 }
