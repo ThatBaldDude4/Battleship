2d4fc9bdc58fed2fbad2c8795e05f533597d94ea
ThatBaldDude4 <taylorv0004@gmail.com>
2026-04-23 13:02:36 -0500

    Add getCordSet() to gameboard
    getCordSet() returns an array containing
    the cordinates the ship can be placed on.

--- game.js
+++ game.js
@@ -20,7 +20,35 @@ class Gameboard {
     constructor() {
         this.board = Array.from({length:10}, () => Array(10).fill({isHit: false}));
     }
+
+    placeShip(ship, cord, direction) {
+
+    };
+
+    getCordSet(shipLength, intialCord, direction) {
+        let set = [];
+        let offset;
+        let startCord = [...intialCord];
+        if (direction.toLowerCase() === "vertical") {
+            offset = [0, 1];
+        };
+        if (direction.toLowerCase() === "horizontal") {
+            offset = [1, 0];
+        };
+
+        for (let i = 0; i < shipLength; i++) {
+            set.push(startCord);
+            startCord = startCord.map((num, index) => {
+                return num + offset[index];
+            })
+        };
+
+        return set;
+    }
 };
 
-let gameBoard = new Gameboard();
-console.log(gameBoard.board);
+let game = new Gameboard();
+
+console.log(game.getCordSet(4, [3, 4], "vertical"));
+
+export {Gameboard};
