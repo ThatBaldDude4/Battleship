988a0348b2a9901f35f69d615d635f61fa25e8d7
ThatBaldDude4 <taylorv0004@gmail.com>
2026-04-23 16:44:40 -0500

    Seperate cordinate cord validation
    Decouple validation logic from getting
    cord logic.

--- game.js
+++ game.js
@@ -26,9 +26,8 @@ class Gameboard {
     }
 
     placeShip(ship, cord, direction) {
-        let cordinates = this.getCordSet(ship.length, cord, direction);
-        if (!cordinates) {throw new Error("invalid ship placement")};
-        console.log(cordinates);
+        let cordinates = this.validateCord(this.getCordSet(ship.length, cord, direction));
+        if (cordinates.length === 0) {return}; // no valid cordinates, early exit
         cordinates.forEach((pair) => {
             this.board[pair[0]][pair[1]].ship = ship;
         });
@@ -38,8 +37,7 @@ class Gameboard {
     // need to validate cordinates to make sure they dont go off board / aren't already occupied
     // getCordSet is coupled with validation logic, seperate out the functions
     // validation also needs to check if spot is already occupied
-    getCordSet(shipLength, intialCord, direction) {
-        if (intialCord[0] > 9 || intialCord[1] > 9) {throw new Error("Cordinate outside board range")};
+    getCordSet(shipLength, intialCord, direction = "vertical") {
         let set = [];
         let offset;
         let startCord = [...intialCord];
@@ -53,19 +51,31 @@ class Gameboard {
         for (let i = 0; i < shipLength; i++) {
             set.push(startCord);
             startCord = startCord.map((num, index) => {
-                let cord = num + offset[index];
-                if (cord > 9) {throw new Error("Cordinate outside board range")}
-                return cord;
+                return num + offset[index];;
             })
         };
+        return set;
+    };
 
-        return set.length === shipLength ? set : null;
+    // checks that cords are in board range
+    // makes sure each cell isnt occupied 
+    validateCord(cordSet) {
+        let intialLength = cordSet.length;
+        let filtered = cordSet.filter(([x, y]) => {
+            return x <= 9 && y <= 9;
+        });
+        filtered = filtered.filter(([x, y]) => {
+            let cell = this.board[x][y];
+            return !Object.hasOwn(cell, "ship");
+        })
+        return filtered.length === intialLength ? filtered : [];
     }
 };
 
 let game = new Gameboard();
 let ship1 = new Ship(5)
 game.placeShip(ship1, [8, 5], "vertical")
+game.placeShip(ship1, [8, 3], "vertical")
 console.log(game.board);
 
 export {Gameboard};
