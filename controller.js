8b92c16dfe4b92ef60df333594086bf9a29dafca
ThatBaldDude4 <taylorv0004@gmail.com>
2026-04-26 20:03:10 -0500

    add controller state
    this is the source of truth for our game
    functions. Its used to dereive game data
    from.

--- controller.js
+++ controller.js
@@ -8,16 +8,28 @@ const player2Board = document.getElementById("player2-board");
 const player1Ships = document.getElementById("player1-ships");
 const player2Ships = document.getElementById("player2-ships");
 
-function controller(pack) {
-    
+const controller = {
+    currentPlayer: null,
+    winner: "null",
+    players: {
+        player1: new Player("Human"),
+        player2: new Player("Computer"),
+    }
+};
+
+function actions(payload) {
+    const oppPlayer = controller.currentPlayer === controller.players.player1 ?
+        controller.players.player2 :
+        controller.players.player1;
 }
 
 function createBoard(board) {
     let html = "";
-    board.forEach((row, y) => {
-        row.forEach((cell, x) => {
+    board.forEach((row, x) => {
+        row.forEach((cell, y) => {
             let ship = cell.ship ? "occupied" : "";
-            html += `<div class="grid-cell ${ship}" data-cord="${[x, y]}"></div>`;
+            let hit = cell.isHit ? "hit" : "";
+            html += `<div class="grid-cell ${ship} ${hit}" data-cord="${[x, y]}"></div>`;
         })
     })
     return html;
@@ -31,17 +43,30 @@ player1Board.addEventListener("click", (e) => {
 
 player2Board.addEventListener("click", (e) => {
     const cords = e.target.dataset.cord; //e.currentTarget for parent container
-    const player = e.currentTarget.dataset.player;
-    console.log({cord: cords, play: player})
+    const board = e.currentTarget.dataset.player;
+    console.log({cord: cords, board: board})
 })
 
 const player1 = new Player("person");
 const player2 = new Player();
 
-player1.gameboard.placeShip(player1.gameboard.ships[0], [3,3])
+player1.gameboard.placeShip(player1.gameboard.ships[0], [9,5])
+player1.gameboard.placeShip(player1.gameboard.ships[1], [8,3])
+player1.gameboard.placeShip(player1.gameboard.ships[2], [2,3]);
+player1.gameboard.placeShip(player1.gameboard.ships[3], [6,5])
+player1.gameboard.placeShip(player1.gameboard.ships[4], [5,5])
 
+player1.gameboard.receiveAttack([9, 5])
+player1.gameboard.receiveAttack([9, 6])
+player1.gameboard.receiveAttack([9, 7])
+player1.gameboard.receiveAttack([9, 1])
+console.log(player1.gameboard.board[9][5])
 let string1 = createBoard(player1.gameboard.board);
 let string2 = createBoard(player2.gameboard.board);
 
 player1Board.innerHTML = string1;
 player2Board.innerHTML = string2;
+
+(() => {
+    controller.currentPlayer = controller.players.player1
+})()
